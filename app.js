747430752e00166c007e02e9f43e7289deb26042
sami855-ux <samitale56@outlook.com>
2024-08-03 10:29:21 +0500

    all static functionality added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const dayInfo = document.querySelector(".wrapper--day")
+const sevenInfo = document.querySelector(".wrapper--sevenday")
+const btns = document.querySelectorAll(".btn")
+const searchBar = document.querySelector(".search--bar")
 
+btns.forEach((btn) => {
+  btn.onclick = function (e) {
+    if (searchBar.value === "") return
+    if (!dayInfo.classList.contains("hidden")) return
+    dayInfo.classList.remove("hidden")
+    sevenInfo.classList.remove("hidden")
+  }
+})
